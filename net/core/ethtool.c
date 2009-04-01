c67272cf08fb88e3a0d76f06630e8ce264599e67
Ionut Nicu <ionut.nicu@ines.ro>
2009-04-01 08:17:08 +0300

    dev_ethtool is needed by lacp
    Signed-off-by: Ionut Nicu <ionut.nicu@ines.ro>

--- net/core/ethtool.c
+++ net/core/ethtool.c
@@ -977,6 +977,7 @@ int dev_ethtool(struct net *net, struct ifreq *ifr)
 	return rc;
 }
 
+EXPORT_SYMBOL(dev_ethtool);
 EXPORT_SYMBOL(ethtool_op_get_link);
 EXPORT_SYMBOL(ethtool_op_get_sg);
 EXPORT_SYMBOL(ethtool_op_get_tso);
